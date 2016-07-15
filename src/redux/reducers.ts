b361685aaebd5cee7c58df7bfabe6bde681e0aaf
Vlad Lebedev <vladlebedeff@gmail.com>
2016-07-15 11:16:30 +0300

    Avoid `var`

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -16,31 +16,46 @@ export function figures(): Reducer<CanvasState> {
     switch(action.type) {
       case Actions.ADD_FIGURE:
         return {
-          figures: [ ...state.figures, (action as Actions.AddFigureAction).figure ]
+          figures: [ ...state.figures, (<Actions.AddFigureAction>action).figure ]
         }
 
       case Actions.REMOVE_FIGURE:
-        var index = (action as Actions.RemoveFigureAction).index;
-        var figures = state.figures;
-        return { figures: figuresExcept(figures, index) }
+        return {
+          figures: figuresExcept(
+            state.figures,
+            (<Actions.RemoveFigureAction>action).index
+          )
+        }
 
       case Actions.PULL_UP_FIGURE:
-        var index = (action as Actions.PullUpFigureAction).index;
-        var figures = state.figures;
-        return { figures: [ ...figuresExcept(figures, index), figures[index] ] }
-
-      case Actions.PUSH_DOWN_FIGURE:
-        var index = (action as Actions.PushDownFigureAction).index;
-        var figures = state.figures;
-        return { figures: [ figures[index], ...figuresExcept(figures, index) ] }
-
-      case Actions.MOVE_FIGURE:
-        var { index, x, y } = (action as Actions.MoveFigureAction);
+        let pull_up_action = (<Actions.PullUpFigureAction>action);
         return {
           figures: [
-            ...state.figures.slice(0, index),
-            new Figure({ shape: state.figures[index].shape, x: x, y: y}),
-            ...state.figures.slice(index + 1)
+            ...figuresExcept(state.figures, pull_up_action.index),
+            state.figures[pull_up_action.index]
+          ]
+        }
+
+      case Actions.PUSH_DOWN_FIGURE:
+        let push_down_action = (<Actions.PushDownFigureAction>action);
+        return {
+          figures: [
+            state.figures[push_down_action.index],
+            ...figuresExcept(state.figures, push_down_action.index)
+          ]
+        }
+
+      case Actions.MOVE_FIGURE:
+        let move_action = (<Actions.MoveFigureAction>action);
+        return {
+          figures: [
+            ...state.figures.slice(0, move_action.index),
+            new Figure({
+              shape: state.figures[move_action.index].shape,
+              x: move_action.x,
+              y: move_action.y
+            }),
+            ...state.figures.slice(move_action.index + 1)
           ]
         }
 
